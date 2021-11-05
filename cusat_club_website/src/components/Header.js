4c291d2f408984268ba1c30ca819ba9423e54ef3
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-11-05 15:52:33 +0530

    events

--- cusat_club_website/src/components/Header.js
+++ cusat_club_website/src/components/Header.js
@@ -1,15 +1,12 @@
 import React from "react";
-import '../arrow.css';
 import { StyleSheet, css } from "aphrodite";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { COLORS } from "../styles/Constants";
 import Logo from "../assets/images/Logo.png";
-import { Link as Link2} from "react-scroll";
+import { Link as Link2 } from "react-scroll";
 
-const Header = () => {
+const Header = ({ event }) => {
 
-	
-	
 	return (
 		<div className={css(styles.root)}>
 			<div className={css(styles.navBar)}>
@@ -19,24 +16,24 @@ const Header = () => {
 				</div>
 				<div className={css(styles.right)}>
 					<div className={css(styles.pageNavs)}>
-						<NavLink exact to="/"  className="links"  activeClassName={css(styles.active_link)}>
-							
+						<NavLink exact to="/" className={css(styles.links)} activeClassName={css(styles.active_link)}>
+							{" "}
 							Home
 						</NavLink>
-						
-						<NavLink exact to="/about" className="links" activeClassName={css(styles.active_link)}>
-							
+						<NavLink exact to="/about" className={css(styles.links)} activeClassName={css(styles.active_link)}>
+							{" "}
 							About
 						</NavLink>
-						<a href="/events" className="links" activeClassName={css(styles.active_link)}>
+						<a href="/events" className={event ? css(styles.link) : css(styles.links)}>
+							{" "}
 							Events
 						</a>
-						<NavLink exact to="/team" className="links" activeClassName={css(styles.active_link)}>
-							
+						<NavLink exact to="/team" className={css(styles.links)} activeClassName={css(styles.active_link)}>
+							{" "}
 							Team
 						</NavLink>
-						<Link2 to="footer" className="links" smooth={true} duration={1500} style={{cursor: "pointer"}}>
-							
+						<Link2 to="footer" className={css(styles.links)} smooth={true} duration={1500} style={{ cursor: "pointer" }}>
+							{" "}
 							Contact Us
 						</Link2>
 					</div>
@@ -67,8 +64,6 @@ const styles = StyleSheet.create({
 
 	active_link: {
 		color: "rgb(22, 165, 150)",
-		borderBottom: "3px solid rgb(22, 165, 150)",
-		// textDecoration: "underline",
 	},
 
 	left: {
@@ -102,6 +97,14 @@ const styles = StyleSheet.create({
 		lineHeight: "30px",
 		fontWeight: "bold",
 		color: "#1B2F5B",
+		marginLeft: 37,
+	},
+	link: {
+		textDecoration: "none",
+		fontSize: 22,
+		lineHeight: "30px",
+		fontWeight: "bold",
+		color: "rgb(22, 165, 150)",
 		marginLeft: 37,
 	},
 });
